e665e23056d055353963746bbee614ca8a1709f2
skaja <skaja@D-SJC-10004780.corp.ebay.com>
2017-03-26 20:46:55 -0700

    commiting search api test code

---
+++ src/main/java/test/ResultResponse.java
@@ -0,0 +1,476 @@
+package test;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class ResultResponse {
+
+	private int resultCount;
+	private String errorMessage;
+	private List<Result> results;
+
+	public int getResultCount() {
+		return resultCount;
+	}
+
+	public void setResultCount(int resultCount) {
+		this.resultCount = resultCount;
+	}
+
+	public List<Result> getResults() {
+		return results;
+	}
+
+	public void setResults(List<Result> results) {
+		this.results = results;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	
+
+	static class Result {
+
+		public String getTrackId() {
+			return trackId;
+		}
+
+		public void setTrackId(String trackId) {
+			this.trackId = trackId;
+		}
+
+		public String getCollectionViewUrl() {
+			return collectionViewUrl;
+		}
+
+		public void setCollectionViewUrl(String collectionViewUrl) {
+			this.collectionViewUrl = collectionViewUrl;
+		}
+
+		public String getFeedUrl() {
+			return feedUrl;
+		}
+
+		public void setFeedUrl(String feedUrl) {
+			this.feedUrl = feedUrl;
+		}
+
+		public String getArtworkUrl30() {
+			return artworkUrl30;
+		}
+
+		public void setArtworkUrl30(String artworkUrl30) {
+			this.artworkUrl30 = artworkUrl30;
+		}
+
+		public String getArtworkUrl60() {
+			return artworkUrl60;
+		}
+
+		public void setArtworkUrl60(String artworkUrl60) {
+			this.artworkUrl60 = artworkUrl60;
+		}
+
+		public String getArtworkUrl100() {
+			return artworkUrl100;
+		}
+
+		public void setArtworkUrl100(String artworkUrl100) {
+			this.artworkUrl100 = artworkUrl100;
+		}
+
+		public String getPreviewUrl() {
+			return previewUrl;
+		}
+
+		public void setPreviewUrl(String previewUrl) {
+			this.previewUrl = previewUrl;
+		}
+
+		public String getWrapperType() {
+			return wrapperType;
+		}
+
+		public void setWrapperType(String wrapperType) {
+			this.wrapperType = wrapperType;
+		}
+
+		public String getKind() {
+			return kind;
+		}
+
+		public void setKind(String kind) {
+			this.kind = kind;
+		}
+
+		public String getArtistId() {
+			return artistId;
+		}
+
+		public void setArtistId(String artistId) {
+			this.artistId = artistId;
+		}
+
+		public String getCollectionId() {
+			return collectionId;
+		}
+
+		public void setCollectionId(String collectionId) {
+			this.collectionId = collectionId;
+		}
+
+		public String getArtistName() {
+			return artistName;
+		}
+
+		public void setArtistName(String artistName) {
+			this.artistName = artistName;
+		}
+
+		public String getCollectionName() {
+			return collectionName;
+		}
+
+		public void setCollectionName(String collectionName) {
+			this.collectionName = collectionName;
+		}
+
+		public String getTrackName() {
+			return trackName;
+		}
+
+		public void setTrackName(String trackName) {
+			this.trackName = trackName;
+		}
+
+		public String getCollectionCensoredName() {
+			return collectionCensoredName;
+		}
+
+		public void setCollectionCensoredName(String collectionCensoredName) {
+			this.collectionCensoredName = collectionCensoredName;
+		}
+
+		public String getTrackCensoredName() {
+			return trackCensoredName;
+		}
+
+		public void setTrackCensoredName(String trackCensoredName) {
+			this.trackCensoredName = trackCensoredName;
+		}
+
+		public Double getCollectionPrice() {
+			return collectionPrice;
+		}
+
+		public void setCollectionPrice(Double collectionPrice) {
+			this.collectionPrice = collectionPrice;
+		}
+
+		public Double getTrackPrice() {
+			return trackPrice;
+		}
+
+		public void setTrackPrice(Double trackPrice) {
+			this.trackPrice = trackPrice;
+		}
+
+		public Date getReleaseDate() {
+			return releaseDate;
+		}
+
+		public void setReleaseDate(Date releaseDate) {
+			this.releaseDate = releaseDate;
+		}
+
+		public Integer getDiscCount() {
+			return discCount;
+		}
+
+		public void setDiscCount(Integer discCount) {
+			this.discCount = discCount;
+		}
+
+		public Integer getDiscNumber() {
+			return discNumber;
+		}
+
+		public void setDiscNumber(Integer discNumber) {
+			this.discNumber = discNumber;
+		}
+
+		public Integer getTrackCount() {
+			return trackCount;
+		}
+
+		public void setTrackCount(Integer trackCount) {
+			this.trackCount = trackCount;
+		}
+
+		public Integer getTrackNumber() {
+			return trackNumber;
+		}
+
+		public void setTrackNumber(Integer trackNumber) {
+			this.trackNumber = trackNumber;
+		}
+
+		public Long getTrackTimeMillis() {
+			return trackTimeMillis;
+		}
+
+		public void setTrackTimeMillis(Long trackTimeMillis) {
+			this.trackTimeMillis = trackTimeMillis;
+		}
+
+		public String getCountry() {
+			return country;
+		}
+
+		public void setCountry(String country) {
+			this.country = country;
+		}
+
+		public String getCurrency() {
+			return currency;
+		}
+
+		public void setCurrency(String currency) {
+			this.currency = currency;
+		}
+
+		public String getPrimaryGenreName() {
+			return primaryGenreName;
+		}
+
+		public void setPrimaryGenreName(String primaryGenreName) {
+			this.primaryGenreName = primaryGenreName;
+		}
+
+		public String getArtistViewUrl() {
+			return artistViewUrl;
+		}
+
+		public void setArtistViewUrl(String artistViewUrl) {
+			this.artistViewUrl = artistViewUrl;
+		}
+
+		public String getTrackViewUrl() {
+			return trackViewUrl;
+		}
+
+		public void setTrackViewUrl(String trackViewUrl) {
+			this.trackViewUrl = trackViewUrl;
+		}
+
+		public double getCollectionHdPrice() {
+			return collectionHdPrice;
+		}
+
+		public void setCollectionHdPrice(double collectionHdPrice) {
+			this.collectionHdPrice = collectionHdPrice;
+		}
+
+		private String trackHdRentalPrice;
+
+		public String getTrackHdRentalPrice() {
+			return trackHdRentalPrice;
+		}
+
+		public void setTrackHdRentalPrice(String trackHdRentalPrice) {
+			this.trackHdRentalPrice = trackHdRentalPrice;
+		}
+
+		public String getHasITunesExtras() {
+			return hasITunesExtras;
+		}
+
+		public void setHasITunesExtras(String hasITunesExtras) {
+			this.hasITunesExtras = hasITunesExtras;
+		}
+
+		public String getLongDescription() {
+			return longDescription;
+		}
+
+		public void setLongDescription(String longDescription) {
+			this.longDescription = longDescription;
+		}
+
+		public String getShortDescription() {
+			return shortDescription;
+		}
+
+		public void setShortDescription(String shortDescription) {
+			this.shortDescription = shortDescription;
+		}
+
+		public String getIsStreamable() {
+			return isStreamable;
+		}
+
+		public void setIsStreamable(String isStreamable) {
+			this.isStreamable = isStreamable;
+		}
+
+		public String getCollectionArtistViewUrl() {
+			return collectionArtistViewUrl;
+		}
+
+		public void setCollectionArtistViewUrl(String collectionArtistViewUrl) {
+			this.collectionArtistViewUrl = collectionArtistViewUrl;
+		}
+
+		public String getCollectionArtistName() {
+			return collectionArtistName;
+		}
+
+		public void setCollectionArtistName(String collectionArtistName) {
+			this.collectionArtistName = collectionArtistName;
+		}
+
+		public String getCollectionArtistId() {
+			return collectionArtistId;
+		}
+
+		public void setCollectionArtistId(String collectionArtistId) {
+			this.collectionArtistId = collectionArtistId;
+		}
+
+		public List<String> getGenreIds() {
+			return genreIds;
+		}
+
+		public void setGenreIds(List<String> genreIds) {
+			this.genreIds = genreIds;
+		}
+
+		public List<String> getGenres() {
+			return genres;
+		}
+
+		public void setGenres(List<String> genres) {
+			this.genres = genres;
+		}
+
+		private List<String> genres;
+
+		public String getArtworkUrl600() {
+			return artworkUrl600;
+		}
+
+		public void setArtworkUrl600(String artworkUrl600) {
+			this.artworkUrl600 = artworkUrl600;
+		}
+
+		public String getContentAdvisoryRating() {
+			return contentAdvisoryRating;
+		}
+
+		public void setContentAdvisoryRating(String contentAdvisoryRating) {
+			this.contentAdvisoryRating = contentAdvisoryRating;
+		}
+
+		public String getTrackExplicitness() {
+			return trackExplicitness;
+		}
+
+		public void setTrackExplicitness(String trackExplicitness) {
+			this.trackExplicitness = trackExplicitness;
+		}
+
+		private String collectionExplicitness;
+
+		public String getCollectionExplicitness() {
+			return collectionExplicitness;
+		}
+
+		public void setCollectionExplicitness(String collectionExplicitness) {
+			this.collectionExplicitness = collectionExplicitness;
+		}
+
+		public String getTrackHdPrice() {
+			return trackHdPrice;
+		}
+
+		public void setTrackHdPrice(String trackHdPrice) {
+			this.trackHdPrice = trackHdPrice;
+		}
+
+		public String getTrackRentalPrice() {
+			return trackRentalPrice;
+		}
+
+		public void setTrackRentalPrice(String trackRentalPrice) {
+			this.trackRentalPrice = trackRentalPrice;
+		}
+
+		private Double trackPrice;
+		private Date releaseDate;
+		private Integer discCount;
+		private Integer discNumber;
+		private Integer trackCount;
+		private Integer trackNumber;
+		private Long trackTimeMillis;
+		private String country;
+		private String currency;
+		private String primaryGenreName;
+		private String artistViewUrl;
+		private String trackViewUrl;
+		private String trackRentalPrice;
+
+		private String wrapperType;
+		private String kind;
+		private String artistId;
+		private String collectionId;
+		private String trackId;
+		private String artistName;
+		private String collectionName;
+		private String trackName;
+		private String collectionCensoredName;
+		private String trackCensoredName;
+
+		private String collectionViewUrl;
+		private String feedUrl;
+
+		private String artworkUrl30;
+
+		private String artworkUrl60;
+		private String artworkUrl100;
+		private Double collectionPrice;
+		private double collectionHdPrice;
+		private String trackHdPrice;
+
+		private String trackExplicitness;
+
+		private String contentAdvisoryRating;
+
+		private String artworkUrl600;
+
+		private List<String> genreIds;
+
+		private String collectionArtistId;
+
+		private String collectionArtistName;
+
+		private String collectionArtistViewUrl;
+		private String isStreamable;
+
+		private String shortDescription;
+
+		private String longDescription;
+
+		private String hasITunesExtras;
+
+		@JsonIgnore
+		private String previewUrl;
+	}
+
+}
---
+++ src/main/java/test/SearchAPITest.java
@@ -0,0 +1,128 @@
+package test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class SearchAPITest {
+	// Test SearchAPI
+
+	String url = "https://itunes.apple.com/search?";
+
+	// search with term itunes
+
+	 @Test
+	@Produces("application/json")
+	public void testSearchbyTerm() throws JsonParseException,
+			JsonMappingException, IOException {
+		Client client = Client.create();
+		String key = "term";
+		String value = "iTunes";
+
+		WebResource webResource = client.resource(
+				"https://itunes.apple.com/search?").queryParam(key, value);
+
+		ClientResponse response = webResource.accept("application/json").get(
+				ClientResponse.class);
+		String res = response.getEntity(String.class);
+		ObjectMapper mapper = new ObjectMapper();
+		ResultResponse result = mapper.readValue(res, ResultResponse.class);
+		Assert.assertEquals(response.getStatus(), 200);
+		Assert.assertEquals(result.getResultCount(), 50);
+
+	}
+
+	// search with no query param
+	@Test
+	public void testSearchbyNoQueryParam() throws JsonParseException,
+			JsonMappingException, IOException {
+
+		Client client = Client.create();
+
+		WebResource webResource = client
+				.resource("https://itunes.apple.com/search?");
+
+		ClientResponse response = webResource.accept("application/json").get(
+				ClientResponse.class);
+
+		String res = response.getEntity(String.class);
+		ObjectMapper mapper = new ObjectMapper();
+		ResultResponse result = mapper.readValue(res, ResultResponse.class);
+		Assert.assertEquals(response.getStatus(), 200);
+		Assert.assertEquals(result.getResultCount(), 0);
+	}
+
+	// search with term with invalid parameter
+	@Test
+	public void testSearchbyTermInvalid() throws JsonParseException,
+			JsonMappingException, IOException {
+
+		Client client = Client.create();
+		String key = "key";
+		String value = "INVALID";
+		WebResource webResource = client.resource(
+				"https://itunes.apple.com/search?").queryParam(key, value);
+
+		ClientResponse response = webResource.accept("application/json").get(
+				ClientResponse.class);
+		String res = response.getEntity(String.class);
+
+		ObjectMapper mapper = new ObjectMapper();
+		ResultResponse result = mapper.readValue(res, ResultResponse.class);
+		Assert.assertEquals(response.getStatus(), 200);
+		Assert.assertEquals(result.getResultCount(), 0);
+	}
+
+	// search with valid term with valid country
+	 @Test
+	public void testSearchbyTermCountry() throws JsonParseException,
+			JsonMappingException, IOException {
+
+		Client client = Client.create();
+		MultivaluedMap<String, String> m = new MultivaluedHashMap<String, String>();
+		m.add("term", "itunes");
+		m.add("country", "US");
+		m.add("media", "music");
+		WebResource webResource = client.resource(
+				"https://itunes.apple.com/search?").queryParams(m);
+
+		ClientResponse response = webResource.accept("application/json").get(
+				ClientResponse.class);
+		String res = response.getEntity(String.class);
+		ObjectMapper mapper = new ObjectMapper();
+		ResultResponse result = mapper.readValue(res, ResultResponse.class);
+		Assert.assertEquals(response.getStatus(), 200);
+		Assert.assertEquals(result.getResultCount(), 50);
+	}
+
+	// search with valid term with invalid country
+	@Test
+	public void testSearchbyTermInValidCountry() throws Exception {
+
+		Client client = Client.create();
+		MultivaluedMap<String, String> m = new MultivaluedHashMap<String, String>();
+		m.add("term", "itunes");
+		m.add("country", "UP");
+
+		WebResource webResource = client.resource(
+				"https://itunes.apple.com/searches?").queryParams(m);
+
+		ClientResponse response = webResource.accept("application/json").get(
+				ClientResponse.class);
+		Assert.assertEquals(response.getStatusInfo().getStatusCode(), 404);
+	}
+
+}
